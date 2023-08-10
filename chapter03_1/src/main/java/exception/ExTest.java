532ecff12010d1a259875ea62888f0846adc4e5a
s2lilac <s2cherryy@gmail.com>
2023-08-10 17:18:16 +0900

    20230810_practice4 commit

---
+++ chapter03_1/src/main/java/exception/ExTest.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class ExTest {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 10 -a; //예외
+//		int b = 11 -a; //정상
+		
+//		int result = (1+2+3)/b;
+
+	}
+	
+}
