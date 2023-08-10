e08a7fbfee6ae46c1c0bced9062c892508f3a708
s2lilac <s2cherryy@gmail.com>
2023-08-10 17:40:25 +0900

    tv practice commit

--- chapter03_1/src/main/java/tv/TV.java
+++ chapter03_1/src/main/java/tv/TV.java
@@ -20,25 +20,66 @@ public class TV {
 	}
 	
 	public void channel(boolean up) {
-		
+		if (up) {
+			if (channel == 255) {
+				this.channel = 1;
+			}
+			else {
+				this.channel++;
+			}
+		} else {
+			if (channel == 1) {
+				this.channel = 255;
+			}
+			else {
+				this.channel--;
+			}
+		}
 	}
 	
 	public void channel(int channel) {
-		this.channel = channel;
+		if (channel >= 1 && channel <=255) {
+			this.channel = channel;
+		} else if(channel <1){
+			this.channel = 255;
+		} else {
+			this.channel= 1;
+		}
 	}
 
 	public void volume(boolean up) {
+		if (up) {
+			if (volume == 100) {
+				this.volume = 0;
+			}
+			else {
+				this.volume++;
+			}
+		} else {
+			if (volume == 0) {
+				this.volume = 100;
+			}
+			else {
+				this.volume--;
+			}
+		}
 		
 	}
 	
-	public void volume(int channel) {
-		this.volume = channel;
+	public void volume(int volume) {
+		if (volume >= 0 && volume <=100) {
+			this.volume = volume;
+		} else if (volume >100){
+			this.volume = 0;
+		} else {
+			this.volume = 100;
+		}
 	}
 	
 	public void status() {
 		System.out.println("TV[power =" + (power? "on" : "off") +
-							"channel " + channel+ "," +
-							"volume " + volume
+							", channel " + channel+ "," +
+							" volume " + volume +  "]"
 								);
 	}
 	
